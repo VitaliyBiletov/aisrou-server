085a9d9df0fce913cf7afa8978661f8c5a31bbe9
VitaliyBiletov <vit.biletov@gmail.com>
2022-04-05 11:49:35 +0300

    Изменения в роутере для смены пароля

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -10,7 +10,7 @@ router.get('/list', checkRole("ADMIN"), userController.getList)
 router.get('/', checkRole("ADMIN"), userController.getUsers)
 router.get('/:id', checkRole("ADMIN"), userController.getUser)
 router.put('/:id', checkRole("ADMIN"), userController.edit)
-router.post('/password', checkRole("ADMIN"), userController.setPassword)
+router.post('/password/', checkRole("ADMIN"), userController.setPassword)
 router.delete('/:id', checkRole("ADMIN"), userController.remove)
 
 router.post('/login', userController.login)
